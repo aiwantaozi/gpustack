10ec0fd43b213acde8f6dd64fdc9d01006f6345c
michelia <michelia@gpustack.ai>
2025-10-20 13:15:26 +0800

    fix: add the missing enum type auth provider

---
+++ gpustack/migrations/versions/2025_10_20_1002-a00ffbca1788_add_authproviderenum.py
@@ -0,0 +1,53 @@
+"""add authproviderenum
+
+Revision ID: a00ffbca1788
+Revises: nrxtab43e8j8
+Create Date: 2025-10-20 10:02:18.323924
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+import gpustack
+from sqlalchemy.dialects import sqlite
+
+# revision identifiers, used by Alembic.
+revision: str = 'a00ffbca1788'
+down_revision: Union[str, None] = 'nrxtab43e8j8'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+auth_providerenum_enum = sa.Enum('Local', 'OIDC', 'SAML', name='authproviderenum')
+bind = op.get_bind()
+dialect = bind.dialect.name
+
+def upgrade() -> None:
+    if dialect == 'postgresql':
+        auth_providerenum_enum.create(bind, checkfirst=True)
+        op.execute("ALTER TABLE users ALTER COLUMN source DROP DEFAULT")
+        op.alter_column('users', 'source',
+            type_=sa.Enum('Local', 'OIDC', 'SAML', name='authproviderenum'),
+            postgresql_using="source::text::authproviderenum"
+        )
+        op.execute("ALTER TABLE users ALTER COLUMN source SET DEFAULT 'Local'")
+    elif dialect == "mysql":
+        op.alter_column('users', 'source',
+            type_=auth_providerenum_enum,
+            existing_type=sa.String(length=255)
+        )
+    # ### end Alembic commands ###
+
+
+def downgrade() -> None:
+    with op.batch_alter_table('users', schema=None) as batch_op:
+        batch_op.alter_column('source',
+            existing_type=sa.Enum('Local', 'OIDC', 'SAML', name='authproviderenum'),
+            type_=sa.VARCHAR(length=255),
+            nullable=False,
+            existing_server_default=sa.text("'Local'"))
+        
+    if dialect == 'postgresql':
+        auth_providerenum_enum.drop(bind, checkfirst=True)
+    # ### end Alembic commands ###

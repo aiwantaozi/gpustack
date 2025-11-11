79c5fc32ac93ac227efe247d3f39e014a931a3c8
michelia <michelia@seal.io>
2025-11-11 20:33:33 +0800

    fix: remigration after server restart

--- pack/s6-overlay/scripts/gpustack-migration.sh
+++ pack/s6-overlay/scripts/gpustack-migration.sh
@@ -4,9 +4,22 @@
 # GPUStack migration oneshot service
 # ================================
 
+STATE_MIGRATION_DONE_FILE="/var/lib/gpustack/run/state_migration_done"
+
 if [ -n "${GPUSTACK_MIGRATION_DATA_DIR}" ]; then
+    if [ -f "$STATE_MIGRATION_DONE_FILE" ]; then
+        echo "[INFO] Migration already completed previously. Skipping."
+        exit 0
+    fi
+
     echo "[INFO] Using GPUSTACK_MIGRATION_DATA_DIR: ${GPUSTACK_MIGRATION_DATA_DIR}."
-    exec gpustack migrate --migration-data-dir "${GPUSTACK_MIGRATION_DATA_DIR}" --database-url 'postgresql://root@localhost:5432/gpustack'
+    if gpustack migrate --migration-data-dir "${GPUSTACK_MIGRATION_DATA_DIR}" \
+        --database-url 'postgresql://root@localhost:5432/gpustack'; then
+        touch "$STATE_MIGRATION_DONE_FILE"
+    else
+        echo "[ERROR] Migration failed."
+        exit 1
+    fi
 else
     echo "[INFO] No migration data dir specified, skipping."
 fi
